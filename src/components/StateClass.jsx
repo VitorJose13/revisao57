e9491c1e098e1cb64c1bba3c212326e7dc2ff489
VitorJose13 <vitorsilvaforte3@gmail.com>
2022-05-27 12:02:04 -0300

    Novo Commit

---
+++ src/components/StateClass.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+export default class StateClass extends React.Component {
+
+    constructor(){
+        super()
+        this.state = {
+            numero: 10
+        }
+    }
+
+    somar(){
+        let soma = this.state.numero + 15
+        this.setState({numero: soma})
+    }
+
+    render() {
+        return(
+            <div>
+                <h1>Componente de classe</h1>
+                <h1> Numero Atual: {this.state.numero} </h1>
+                <button onClick={
+                    () => this.somar()
+                }>
+                    Somar
+                </button>
+            </div>
+        )
+    }
+
+}
